5e4714122cfefae2e351dc07d598fb37cf4aa711
a_dido97 <ahmed.benyamin2015@gmail.com>
2025-08-20 11:38:24 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'motor_serial_test'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,13 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='adido97',
-    maintainer_email='ahmed.benyamin2015@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer='ahmad',
+    maintainer_email='ahmad@example.com',
+    description='Simple ROS2 package to test motor serial communication with Arduino.',
+    license='BSD',
     entry_points={
         'console_scripts': [
+            'motor_driver = motor_serial_test.motor_driver:main',
+            'motor_gui = motor_serial_test.motor_gui:main',
         ],
     },
 )
